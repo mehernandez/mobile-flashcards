3396e4416c22122ccf04e60f00e5756af188191a
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2018-04-26 13:23:32 -0500

    Finish UI for deck view

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { Header, Card, Button } from 'react-native-elements';
+import ActionButton from 'react-native-action-button';
 
 export default class App extends React.Component {
+
   render() {
+
+    let decks = [{ name: 'SAP', card_count: 10, key: '0' }, { name: 'Python', card_count: 3, key: '1' }, { name: 'SAP', card_count: 10, key: '2' }, { name: 'Python', card_count: 3, key: '3' }, { name: 'SAP', card_count: 10, key: '4' }, { name: 'Python', card_count: 3, key: '5' }];
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header
+          centerComponent={{ text: 'Udacicards', style: styles.header }}
+        />
+        <FlatList
+          style={{ flex: 1 }}
+          data={decks}
+          ListHeaderComponent={<Text style={styles.listTitle}>Decks</Text>}
+          ListEmptyComponent={<Text>No decks yet</Text>}
+          renderItem={({ item }) =>
+            (
+              <TouchableOpacity
+                onPress={() => alert(item)}>
+                <Card title={item.name}>
+                  <Text>{item.card_count} cards</Text>
+                </Card>
+              </TouchableOpacity>
+            )}
+        />
+        <ActionButton
+          buttonColor="rgba(231,76,60,1)"
+          onPress={() => { alert("Add deck") }}
+        />
       </View>
     );
   }
@@ -16,8 +41,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column'
   },
+  header: {
+    fontSize: 25,
+    color: '#fff',
+    paddingTop: 15
+  },
+  listTitle: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#545454'
+  }
 });
